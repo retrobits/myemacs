02edc818287b8699eb6e60e61e5f97cdbb39741a
dtvernon <dtvernon@b987d642-4c33-0410-b914-bfbeeca97dc5>
2007-06-21 14:43:45 +0000

    hmmm...

---
+++ trunk/base.el
@@ -0,0 +1,74 @@
+(put 'dired-find-alternate-file 'disabled nil)
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(nxhtml-load t))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(default ((t (:stipple nil :background "SystemWindow" :foreground "SystemWindowText" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 105 :width normal :family "Consolas")))))
+
+;;(add-to-list 'load-path "~/elisp/ruby")
+
+(global-set-key "\C-x\C-m" 'execute-extended-command)
+(global-set-key "\C-c\C-m" 'execute-extended-command)
+(global-set-key "\C-w" 'backward-kill-word)
+(global-set-key "\C-x\C-k" 'kill-region)
+(global-set-key "\C-c\C-k" 'kill-region)
+
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+
+(defun w32-restore-frame ()
+    "Restore a minimized frame"
+     (interactive)
+     (w32-send-sys-command 61728))
+
+(defun w32-maximize-frame ()
+    "Maximize the current frame"
+     (interactive)
+     (w32-send-sys-command 61488))
+
+(w32-maximize-frame)
+
+(defalias 'qrr 'query-replace-regexp)
+(defalias 'lml 'list-matching-lines)
+
+(when (eql system-type 'windows-nt) ; Windows
+      (when (eq (shell-command "ruby") 0) 
+        ; !WARNING! ugly hack because of 1.8.4-20
+        ;(setq config-rubyelispdir
+    	;  "misc" ) 
+        ; get the installation directory
+        ;(setq config-rubydir "/cygwin/usr/src/ruby-1.8.6")
+    	;  (substring 
+    	;   (shell-command-to-string
+    	;    "ruby -rrbconfig -e 'puts Config::CONFIG[ \"exec_prefix\"]'") 0 -1)) 
+        ; add the emacs lisp directory so emacs acan find it
+        (add-to-list 'load-path "/cygwin/usr/src/ruby-1.8.6-1/misc")
+	      ;(append
+    	;	     (list (expand-file-name 
+    		;	   config-rubyelispdir config-rubydir))
+    		 ;    load-path))
+        ;; define autoloads
+        ;; from inf-ruby.el
+        (autoload 'ruby-mode "ruby-mode" 
+          "Mode for editing ruby source files" t)
+        (setq auto-mode-alist
+    	  (append '(("\\.rb$" . ruby-mode)
+    		    ("[Rr]akefile" . ruby-mode))
+    		  auto-mode-alist))
+        (setq interpreter-mode-alist (append '(("ruby" . ruby-mode))
+    					 interpreter-mode-alist))
+        (autoload 'run-ruby "inf-ruby"
+          "Run an inferior Ruby process")
+        (autoload 'inf-ruby-keys "inf-ruby"
+          "Set local key defs for inf-ruby in ruby-mode")
+        (add-hook 'ruby-mode-hook
+    	      '(lambda ()
+    		 (inf-ruby-keys)))))
---
+++ trunk/casting.el
@@ -0,0 +1,72 @@
+(setq eval-expression-print-length nil)
+(setq objects '(whiskey-bottle bucket frog chain))
+
+(setq map '((living-room (you are in the living room
+                of a wizards house - there is a wizard
+                snoring loudly on the couch -)
+                (west door garden)
+                (upstairs stairway attic))
+              (garden (you are in a beautiful garden -
+                there is a well in front of you -) 
+                (east door living-room))
+              (attic (you are in the attic of the
+                wizards house - there is a giant
+                welding torch in the corner -)
+                (downstairs stairway living-room)))) 
+
+(setq object-locations '((whiskey-bottle living-room)
+                           (bucket living-room)
+                           (chain garden)
+                           (frog garden)))
+
+(setq location 'living-room) 
+
+(defun describe-location (location map)
+    (second (assoc location map)))
+
+(describe-location 'living-room map)
+
+(defun describe-path (path)
+  `(there is a ,(second path) going ,(first path) from here.)) 
+
+(describe-path '(west door garden))
+
+(defun describe-paths (location map)
+  (apply #'append (mapcar #'describe-path (cddr (assoc location map)))))
+
+(describe-paths 'living-room map)
+
+(defun is-at (obj loc obj-loc)
+  (eq (second (assoc obj obj-loc)) loc))
+
+(is-at 'whiskey-bottle 'living-room object-locations)
+
+(defun describe-floor (loc objs obj-loc)
+  (apply #'append (mapcar (lambda (x) 
+                            `(you see a ,x on the floor.))
+                          (remove-if-not (lambda (x)
+                                           (is-at x loc obj-loc))
+                                         objs)))) 
+
+(describe-floor 'living-room objects object-locations)
+
+
+
+(defun look ()
+  (append (describe-location location map)
+          (describe-paths location map)
+          (describe-floor location objects object-locations))) 
+
+(look)
+
+
+(defun walk-direction (direction)
+  (let ((next (assoc direction (cddr (assoc location map)))))
+    (cond (next (setf location (third next)) (look))
+      (t '(you cant go that way.))))) 
+
+(walk-direction 'west)
+(walk-direction 'east)
+(walk-direction 'upstairs)
+(walk-direction 'downstairs)
+(look) 
