ac99caec73019bb2f8171cb9753b0558b79ff35a
dtvernon <dtvernon@b987d642-4c33-0410-b914-bfbeeca97dc5>
2007-07-20 21:22:37 +0000

    slime w32 fix

--- trunk/emacs/mylisp/slime.lisp
+++ trunk/emacs/mylisp/slime.lisp
@@ -20,3 +20,25 @@
 	                :dont-close t
 			:coding-system (or encoding-fmt "latin-1"))
 
+#| a C# routine to manage the allegro windows, todo: convert to lisp
+
+        public void Cleanup()
+        {
+            int waitMs = 200;
+            int consoleHwnd = 0;
+            _stop = new ManualResetEvent(false);
+            _stop.WaitOne(waitMs, false);
+
+            int hwnd = Win32.FindWindow(null, "cleaner.exe");
+            if (hwnd != 0) Win32.ShowWindow(hwnd, 0); 
+
+            while (!_stop.WaitOne(waitMs, false))
+            {
+                hwnd = Win32.FindWindow("Allegro CL Free Express", "Allegro CL Free Express");
+                if (hwnd != 0) Win32.SendMessage(hwnd, Win32.WM_KEYDOWN, 0x1b, 0x00010001); // send Esc
+
+                hwnd = Win32.FindWindow("WebclosFrame", "Allegro Common Lisp Console - [mlisp.dxl]");
+                if (hwnd != 0 && consoleHwnd != hwnd) consoleHwnd = hwnd; Win32.ShowWindow(hwnd, 0);
+            }
+        }
+|#
